198cfe09c5c359390a2ac419f59ee40dd1971ae8
aarontbarratt <aarontbarratt@gmail.com>
2023-10-01 21:14:19 +0100

    mod file

--- go.mod
+++ go.mod
@@ -1,6 +1,5 @@
 module example.com
 
-go 1.21.0
+go 1.21.1
 
-require github.com/banaaron/gotils v0.0.3 // indirect
-
+require github.com/banaaron/gotils v0.0.3
--- main.go
+++ main.go
@@ -7,7 +7,16 @@ import (
 
 func main() {
 	res := gotils.LeftPad("Hello, World!", 4)
+	fmt.Println(res)
+
 	var err error = nil
 	gotils.HandleError(err)
-	fmt.Printf(res)
+
+	m := map[int]string{
+		1: "aaron",
+		2: "chris",
+		3: "drew",
+	}
+	keys := gotils.OrderedKeys(m)
+	fmt.Println(keys)
 }
